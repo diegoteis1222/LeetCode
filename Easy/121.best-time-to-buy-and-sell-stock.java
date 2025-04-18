2ea49b2a8d5d819fb6a59be3886fd093a4e0111f
diegoteis122 <diegoteis122@gmail.com>
2025-04-18 22:47:08 +0200

    echo el ejercicio 121

---
+++ Easy/121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,33 @@
+package Easy;
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+    static public int maxProfit(int[] prices) {
+
+        int lower = prices[0];
+        int max = 0;
+        int temp = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < lower) {
+                lower = prices[i];
+                temp = i;
+            }
+        }
+
+        for(int h = temp; h < prices.length; h++) {
+            if (prices[h] > max) {
+                max = prices[h];
+            }
+        } 
+
+        return max - lower;
+    }
+}
+// @lc code=end
+
