42b721a921fe58a8378e3077c9dc2132b141f912
diegoteis122 <diegoteis122@gmail.com>
2025-04-20 01:25:39 +0200

    resuelto el problema 181

--- pruebas.java
+++ pruebas.java
@@ -1,3 +1,34 @@
 public class pruebas {
 
+    // clase ListNode
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    static public int hammingWeight(int n) {
+        
+        String paco = Integer.toBinaryString(n);
+
+        System.out.println(paco);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        hammingWeight(11);
+    }
+
 }
