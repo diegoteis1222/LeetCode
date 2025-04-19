59d040a2b5b44ee720e99dcbdbe53cbd48908321
diegoteis122 <diegoteis122@gmail.com>
2025-04-20 01:49:06 +0200

    echo el ejercicio 338

---
+++ Easy/338.counting-bits.java
@@ -0,0 +1,33 @@
+package Easy;
+/*
+ * @lc app=leetcode id=338 lang=java
+ *
+ * [338] Counting Bits
+ */
+
+// @lc code=start
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] bin = new int[n + 1]; // creamos nuevo array
+
+        for (int i = 0; i <= n; i++) { // recorremos el array tantas veces como n sea
+
+            int cont = 0; // contador
+            int num = i; // recogemos el valor de i 
+
+            while (num > 0) { // minetras el numero no sea 0
+                int bit = num % 2; // el bit es el resto de dividir el numero entre 2
+                if (bit == 1) {
+                    cont++; // si el bit es 1 suma uno al contador
+                }
+                num /= 2; // divide el numero entre 2
+            }
+
+            bin[i] = cont; // a la posicion del nuevo array en i le damos el valor del contador
+        }
+
+        return bin;
+    }
+}
+// @lc code=end
